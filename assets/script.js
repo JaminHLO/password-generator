435cf1f5dc7925f82493c48af954a187f3c6ab9e
Jaimon <jamin.hogan@gmail.com>
2023-02-06 23:47:15 -0500

    added code to generate randomized password

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,19 @@
 // Assignment code here
 
-//define global variables for password characteristics
-// var pwLength = 0 // must be between 8 and 128 characters
-// var pwUpper = false;
-// var pwLower = false;
-// var pwNumeric = false;
-// var pwSpecial = false;
+//define global variable for password characteristics
 var passwordInfo = {pwLength: 0, pwLower: false, pwUpper: false, 
   pwNumeric: false, pwSpecial: false
 };
 
 console.log(passwordInfo);
 
-function generatePassword(){
-
+//collect user password specifications
+function collectPasswordSpecs (){
   //collect valid password length
   passwordInfo.pwLength = this.prompt("How long should the password be? \n(Must be between 8 and 128 characters)");
   if (passwordInfo.pwLength < 8 || passwordInfo.pwLength > 128) {
     this.alert ("Error: Password must be between 8 and 128 characters in length");
-    return;
+    return false;
   }
 
   //confirm whether user would like lowercase
@@ -39,10 +34,44 @@ function generatePassword(){
     passwordInfo.pwNumeric === false &&
     passwordInfo.pwSpecial === false ) {
     this.alert ("Error: Password must include at least one of the following types: \nLowercase, Uppercase, Numeric, or Special");
-    return;
+    return false;
   }
+  return(true);
+}
+
+
+function generatePassword(){
+  console.log(passwordInfo);
+  var pwCharList = '';
+  var passwordFinished = '';
+  if (passwordInfo.pwLower === true) {
+    pwCharList = pwCharList.concat('abcdefghijklmnopqrstuvwxyz');
+    // console.log(pwCharList);
+  }
+  if (passwordInfo.pwUpper === true) {
+    pwCharList = pwCharList.concat('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    // console.log(pwCharList);
+  }
+  if (passwordInfo.pwNumeric === true) {
+    pwCharList = pwCharList.concat('1234567890');
+    // console.log(pwCharList);
+  }
+  if (passwordInfo.pwSpecial === true) {
+    pwCharList = pwCharList.concat('!#$%&*+-<=>?@^~');
+    //  console.log(pwCharList);
+  }
+  console.log("Chars:", pwCharList, "\nLength of Possibles:", pwCharList.length);
   
-  return(console.log(passwordInfo));
+  //generate random number with user defined length
+  for (i=0; i<passwordInfo.pwLength; i++) {
+    // Get random index from string of character possibilities (pwCharList)
+    var index = Math.floor(Math.random() * pwCharList.length);
+    // add this random possible character to our password
+    passwordFinished = passwordFinished.concat(pwCharList[index]);
+  }
+  //return our finished password string
+  console.log(passwordFinished);
+  return (passwordFinished);
 }
 
 // Get references to the #generate element
@@ -50,6 +79,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  if (!collectPasswordSpecs()){
+    return;
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
